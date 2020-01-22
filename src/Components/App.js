9b3b8c44b44b0f4e6fe0083da64ca98447776526
rednjzz <rednjzz@gmail.com>
2020-01-22 22:29:38 +0900

    add Query

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,10 +3,26 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-      <GlobalStyles />
-      <Router isLoggedIn={true} />
-  </ThemeProvider>
-)
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
