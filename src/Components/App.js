6e1a83980024e0022af3f4a62ce991a65d7a76b6
rednjzz <rednjzz@gmail.com>
2020-01-22 21:35:41 +0900

    Router setup

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import Router from "./Router";
+
+export default () => (
+  <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      <Router isLoggedIn={true} />
+  </ThemeProvider>
+)
